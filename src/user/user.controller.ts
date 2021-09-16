ce1e655910127367d30bb6b3b098bd221e0fe779
Tomasz Janicki <t.janicki88@gmail.com>
2021-09-16 18:34:41 +0200

    user active

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Get,
+  Param,
   Patch,
   Post,
   Put,
@@ -15,6 +16,7 @@ import { UserDeviceGuard } from '../auth/user-device.guard';
 import { UserDto } from './user.dto';
 import { UpdateUserDto } from './update-user.dto';
 import { UpdatePasswordDto } from '../auth/update-password.dto';
+import { RoleEntity } from './role.entity';
 
 @Controller('/users')
 export class UserController {
@@ -58,9 +60,9 @@ export class UserController {
     };
   }
 
-  @Put()
-  async changeUserInfo(@Request() req, @Body() dto: UpdateUserDto) {
-    const user = await this.userService.updateUser(req.user.userId, dto);
+  @Put('/me')
+  async changeInfoAboutMe(@Request() req, @Body() dto: UpdateUserDto) {
+    const user = await this.userService.updateInfoAboutMe(req.user.userId, dto);
     return {
       id: user.id,
       firstName: user.firstName,
@@ -68,11 +70,29 @@ export class UserController {
       email: user.email,
       isActive: user.isActive,
       createdAt: user.createdAt,
-      roles: user.roles.map((r) => r.name),
+      roles: user.roles.map((r: RoleEntity) => r.name),
     };
   }
 
-  @Get('/profile')
+  @Put('/:userId')
+  async changeUserInfo(
+    @Param('userId') userId: string,
+    @Body() dto: UpdateUserDto,
+  ) {
+    console.log('dto', dto);
+    const user = await this.userService.updateUser(userId, dto);
+    return {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      isActive: user.isActive,
+      createdAt: user.createdAt,
+      roles: user.roles.map((r: RoleEntity) => r.name),
+    };
+  }
+
+  @Get('/me')
   async getProfile(@Request() req): Promise<UserDto> {
     const user = await this.userService.getByIdOrThrow(req.user.userId);
     // todo factory or find mapper
